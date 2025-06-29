52ba8d7d46166d829ef52a1dd26e5e91b12b4ea3
Esteban-J-Barolo <estebanjavierbarolo@gmial.com>
2025-06-29 02:03:33 -0300

    arch llm

--- llm_interface.py
+++ llm_interface.py
@@ -6,11 +6,7 @@ class Llm:
     def __init__(self):
         self.modelo = 'nvidia/llama-3.3-nemotron-super-49b-v1:free'
         self.temperature = 0.7
-<<<<<<< HEAD
-        self.API_KEY = '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' # https://openrouter.ai/settings/keys
-=======
         self.API_KEY = '' # https://openrouter.ai/settings/keys
->>>>>>> 95974e349d44416d651f5d739f0699563c6aed86
         self.url = "https://openrouter.ai/api/v1/chat/completions"
         # self.client = OpenAI(
         #     base_url="https://openrouter.ai/api/v1",
